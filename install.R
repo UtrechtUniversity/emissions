b55d1b0f9c37df16e557b64e35867be3d856f9f7
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-02-06 14:56:34 +0100

    Add installation script

---
+++ install.R
@@ -0,0 +1,18 @@
+# install.R
+# 
+# Author: Jonathan de Bruin and Barbara Vreede
+# Project: Emissions in The Netherlands in 2030
+# Creation date: 2019-02-06
+# Modification date: 2019-02-06
+# 
+# Description: Use this script to install required dependencies
+# for the project.
+#
+
+# install packages for data manipulation
+install.packages('dplyr')
+
+# install packages for visualisation
+install.packages('ggplot2')
+
+# install packages for statistics
