101e97bbeb4c2b661f1681761f523b13d65ffd14
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-02-20 15:44:31 +0100

    Add a forecast to predict CO2 emission

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -6,17 +6,25 @@
 # Modification date: 2018-09-25
 # 
 # Description: This script does the analysis on the clean data. 
-# We estimated the emission for 2020 with this script.
+# We estimated the emission for 2030 with this script.
 #
 
 
 # import required libraries
-library(ggplot2)
+library(readr)
 library(dplyr)
+library(ggplot2)
+
+# create the folders to store the results
+dir.create(
+  file.path("output", "figs"),
+  recursive = TRUE,
+  showWarnings = FALSE
+)
 
 # read the clean dataset in from the output folder
-data_fp <- file.path("output", "emissions_to_air_190918144657_clean.csv")
-data <- read.csv2(data_fp)
+data_fp <- file.path("output", "emissions.csv")
+data <- read_csv2(data_fp)
 
 # focus on the totals
 data_totals <- data %>% 
@@ -24,22 +32,45 @@ data_totals <- data %>%
   arrange(Period)
 
 # plot emission for
-data_totals %>% 
-  ggplot(aes(Period, CO2.emission)) + geom_line() + 
-  ggsave(file.path("figs", "CO2_total.png"))
+(emissions_plot <- data_totals %>% 
+  ggplot(aes(Period, CO2_emission)) + 
+    geom_line() + labs(
+      title = "Forecast of CO2 emission in The Netherlands.", 
+      x = "Year", 
+      y = "Emission [mln kgs]"))
 
-# estimate the emission for 2020
-# Bad statistics <3
+# save the plot to a file in the output/figs folder
+emissions_plot + ggsave(file.path("output", "figs", "CO2_total.png"))
 
-a <- (data_totals$CO2.emission[nrow(data_totals)] - data_totals$CO2.emission[1]) /
-  (data_totals$Period[nrow(data_totals)] - data_totals$Period[1])
+# estimate the emission for 2030
 
-est_CO2_2020 <- data_totals$CO2.emission[nrow(data_totals)] + a * (2020 - data_totals$Period[nrow(data_totals)])
-print(est_CO2_2020)
+library(forecast)
+
+# convert the data into a time series.
+ts_co2_emissions <- data_totals %>% 
+  filter(Period >= 1990) %>% 
+  pull(CO2_emission) %>% 
+  ts(frequency=1, start= 1990)
+
+# fit an arima model with drift to the time series
+fit_co2_emissions <- auto.arima(ts_co2_emissions, seasonal=FALSE)
+
+# Perform a forcast on the data
+fcast_co2_emissions <- forecast::forecast(fit_co2_emissions, h=13)
+
+# plot the results and save the plot to a file.
+(fcast_plot <- autoplot(fcast_co2_emissions) + 
+  geom_forecast(h=13) + 
+  labs(
+    title = "Forecast of CO2 emission in The Netherlands.", 
+    x = "Year", 
+    y = "Emission [mln kgs]"))
+
+fcast_plot + ggsave(file.path("output", "figs", "forecast_CO2_emission.png"))
 
 # write result to file.
 write.csv(
-  data.frame(est_CO2_2020),
-  file.path("output", "estimation_CO2_2020.csv")
+  fcast_co2_emissions,
+  file.path("output", "prediction_CO2_2030.csv")
 )
 
