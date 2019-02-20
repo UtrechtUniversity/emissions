69f53e558a70e2ba9067040555c25f99117fda1e
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-02-20 16:33:24 +0100

    Remove clean file (are we about to include this?)

--- clean.R
+++
@@ -1,7 +0,0 @@
-# remove output folders so all previous output is empty
-unlink("output", recursive = T)
-unlink("figs", recursive = T)
-
-# create output folders
-dir.create("output")
-dir.create("figs")
